2bab4e57b3d8d44f774c8f6793c0d519f9c7cbac
Antoine Sterna <antoine.sterna@teamwork.net>
2022-12-06 11:38:48 +0100

    (feat) Added Backup for vmconfidentiel

---
+++ backup.tf
@@ -0,0 +1,29 @@
+resource "azurerm_recovery_services_vault" "rsv" {
+  name                = "recovery-vault"
+  location            = azurerm_resource_group.rg_confidentiel.location
+  resource_group_name = azurerm_resource_group.rg_confidentiel.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_backup_policy_vm" "rvp" {
+  name                = "recovery-vault-policy"
+  resource_group_name = azurerm_resource_group.rg_confidentiel.name
+  recovery_vault_name = azurerm_recovery_services_vault.rsv.name
+
+  backup {
+    frequency = "Daily"
+    time      = "23:00"
+  }
+}
+
+data "azurerm_virtual_machine" "vmconf" {
+  name                = "vmconfidentiel"
+  resource_group_name = azurerm_resource_group.rg_confidentiel.name
+}
+
+resource "azurerm_backup_protected_vm" "backupvmconf" {
+  resource_group_name = azurerm_resource_group.rg_confidentiel.name
+  recovery_vault_name = azurerm_recovery_services_vault.rsv.name
+  source_vm_id        = data.azurerm_virtual_machine.vmconf.id
+  backup_policy_id    = azurerm_backup_policy_vm.rvp.id
+}
