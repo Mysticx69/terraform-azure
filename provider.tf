9c1b9bb8d1e39b20109d8f014cdf893fd625dd31
Antoine Sterna <sternaantoine@gmail.com>
2022-12-10 17:26:53 +0100

    (feat) Added recover key permisision on kevaults

--- provider.tf
+++ provider.tf
@@ -27,7 +27,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy       = true
+      purge_soft_deleted_keys_on_destroy = true
+    }
+  }
 }
 
 
