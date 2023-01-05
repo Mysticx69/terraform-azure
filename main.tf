37737007454308141df5d8db8cead844ba37cbf2
Antoine Sterna <antoine.sterna@teamwork.net>
2023-01-05 16:42:35 +0100

    (feat) Added log analytics workspaces

--- main.tf
+++ main.tf
@@ -18,3 +18,22 @@ resource "azurerm_resource_group" "rg_confidentiel" {
     name = "Confidentiel Resource Group"
   })
 }
+
+##############################################################
+# Log Analytics Workspace
+##############################################################
+resource "azurerm_log_analytics_workspace" "wsc" {
+  name                = "Workspace_confidentiel"
+  location            = azurerm_resource_group.rg_confidentiel.location
+  resource_group_name = azurerm_resource_group.rg_confidentiel.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_log_analytics_workspace" "wss" {
+  name                = "Workspace_system"
+  location            = azurerm_resource_group.rg_system.location
+  resource_group_name = azurerm_resource_group.rg_system.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
